65fda5a3f9633f3a45ee4a819a80190d62ebe535
Pedro Crelier <torialitym@gmail.com>
2025-09-10 19:00:10 -0300

    Serverless mongodb

--- src/utils/connectDB.ts
+++ src/utils/connectDB.ts
@@ -1,13 +1,47 @@
-import mongoose from "mongoose";
-import logger from "./logger.js";
+import mongoose, { type ConnectOptions, Mongoose } from "mongoose";
 import env from "../config/env.js";
 
-export async function connectDB() {
-  try {
-    await mongoose.connect(env.ATLAS_URI);
-    logger.info("MongoDB connected successfully");
-  } catch (err) {
-    logger.error({ err }, "MongoDB connection error");
-    process.exit(1);
-  }
+declare global {
+  // eslint-disable-next-line no-var
+  var mongoose: any; // This must be a `var` and not a `let / const`
 }
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connectDB(): Promise<Mongoose> {
+  const MONGODB_URI = env.ATLAS_URI;
+
+  if (!MONGODB_URI) {
+    throw new Error(
+      "Please define the MONGODB_URI environment variable inside .env.local",
+    );
+  }
+
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts: ConnectOptions = {
+      bufferCommands: false,
+      maxPoolSize: 10,
+      minPoolSize: 1,
+      serverSelectionTimeoutMS: 5000,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then(() => mongoose);
+  }
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+
+  return cached.conn;
+}
+
