f2fb439f4e66666ce49cb43b175a2f6810035db4
Pedro Crelier <torialitym@gmail.com>
2025-09-10 18:45:23 -0300

    Changes to make it work with vercel hosting

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -6,21 +6,20 @@ const baseOptions = {
   timestamp: () => `,"time":"${new Date().toISOString()}"`,
 };
 
-const devLogger = pino({
-  ...baseOptions,
-  transport: {
-    target: "pino",
-    options: {
-      colorize: true,
-      levelFirst: false,
-      translateTime: "HH:MM:ss",
-      ignore: "pid,hostname",
-    },
-  },
-});
-
-const prodLogger = pino(baseOptions);
-
-const logger = env.NODE_ENV === "production" ? prodLogger : devLogger;
+const logger =
+  env.NODE_ENV === "production"
+    ? pino(baseOptions)
+    : pino({
+        ...baseOptions,
+        transport: {
+          target: "pino-pretty",
+          options: {
+            colorize: true,
+            levelFirst: false,
+            translateTime: "HH:MM:ss",
+            ignore: "pid,hostname",
+          },
+        },
+      });
 
 export default logger;
